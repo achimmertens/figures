882dbe7461b7f5ab6bdb34d300acf15af21ed56f
Achim Mertens <achim_mertens@gmx.de>
2023-01-10 07:43:29 +0100

    Den Inhalt von Figures mit dem von angular-canvasjs-chart-samples ausgetauscht

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'figures'`, () => {
+  it(`should have as title 'angular-routing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('figures');
+    expect(app.title).toEqual('angular-routing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('figures app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('angular-routing');
   });
 });
